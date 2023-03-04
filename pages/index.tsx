bf3566deec56986a56d5378bc8c56e2e18309254
yu-yaba <kawai.yuya3401@gmail.com>
2023-03-04 19:38:21 +0900

    fix button design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,64 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import { GetServerSideProps, NextPage } from "next";
+import { useState } from "react";
+import styles from "./index.module.css";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-)
+type Props = {
+  initialImageUrl: string;
+};
 
-export default IndexPage
+const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
+  // ❶ useStateを使って状態を定義する
+  const [imageUrl, setImageUrl] = useState(initialImageUrl);
+  const [loading, setLoading] = useState(false);
+  // ❷ マウント時に画像を読み込む宣言
+  // useEffect(() => {
+  //   fetchImage().then((newImage) => {
+  //     setImageUrl(newImage.url); // 画像URLの状態を更新する
+  //     setLoading(false); // ローディング状態を更新する
+  //   });
+  // }, []);
+  const handleClick = async () => {
+    setLoading(true);
+    const newImage = await fetchImage();
+    setImageUrl(newImage.url);
+    setLoading(false);
+  };
+  // ❸ ローディング中でなければ、画像を表示する
+  return (
+  <div className={styles.page}>
+      <button
+        onClick={handleClick}
+        style={{
+          backgroundColor: "#319795",
+          border: "none",
+          borderRadius: "4px",
+          color: "white",
+          padding: "4px 8px",
+        }}
+      >
+        きょうのにゃんこ🐱
+      </button>    {loading || <img src={imageUrl} className={styles.img}/>}
+    </div>
+  );
+};
+export default IndexPage;
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const image = await fetchImage();
+  return {
+    props: {
+      initialImageUrl: image.url,
+    },
+  };
+};
+
+type Image = {
+  url: string;
+};
+const fetchImage = async (): Promise<Image> => {
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const images = await res.json();
+  console.log(images);
+  return images[0];
+};
+
